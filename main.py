bd6ecbdc8dd1f7446b133f18a3087a90a08bfecd
Erick Gabriel <erickgabrielferreira@alu.ufc.br>
2025-05-08 15:59:43 -0300

    Create pay_roll crud
    Create pay_roll_service
    
    Add uvicorn function for main
    
    Change methods in employee_service to english

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 from routes.department_routes import router as department_router
 from routes.employee_routes import router as employee_router
+from routes.pay_roll_routes import router as payroll_router
 
 app = FastAPI()
 
 app.include_router(department_router)
 app.include_router(employee_router)
+app.include_router(payroll_router)
 
 if __name__=="__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
