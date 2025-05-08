dfa98209a3b14fd711c3a5d236ef740b0da3d80b
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2025-05-07 21:30:29 -0300

    fix: removing end pay_roll end pints

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 from routes.department_routes import router as department_router
 from routes.employee_routes import router as employee_router
-from routes.pay_roll_routes import router as pay_roll_router
 
 app = FastAPI()
 
 app.include_router(department_router)
 app.include_router(employee_router)
-app.include_router(pay_roll_router)
